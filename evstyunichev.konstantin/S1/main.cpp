96c63b96ffabd69ee7f519f293a3932f68b4ed45
evstyunichev.konstantin <korombodis.gmail.com>
2025-02-17 14:33:53 +0300

    standart list version

--- evstyunichev.konstantin/S1/main.cpp
+++ evstyunichev.konstantin/S1/main.cpp
@@ -1,4 +1,67 @@
+#include <iostream>
+#include <iterator>
+#include <fstream>
+#include <list>
+#include <string>
+
+using pair_t = std::pair< std::string, std::list< int > >;
+
+
 int main()
 {
-  
+  std::fstream fin("input.txt");
+  std::list< pair_t > sequences;
+  std::list< long long > summ;
+  std::string str;
+  bool flag = false;
+  while (fin >> str)
+  {
+    if (isalpha(str[0]))
+    {
+      sequences.push_back({str, std::list< int >{}});
+    }
+    else
+    {
+      sequences.back().second.push_back(std::stoi(str));
+      flag = true;
+    }
+  }
+  auto iter_seq = sequences.begin();
+  std::cout << (iter_seq++)->first;
+  for (; iter_seq != sequences.end(); ++iter_seq)
+  {
+    std::cout << ' ' << iter_seq->first;
+  }
+  if (!flag)
+  {
+    std::cout << '\n';
+  }
+  while (flag)
+  {
+    std::cout << '\n';
+    flag = false;
+    for (iter_seq = sequences.begin(); iter_seq != sequences.end(); ++iter_seq)
+    {
+      if (!(*iter_seq).second.empty())
+      {
+        if (!flag)
+        {
+          summ.push_back(0ll);
+        }
+        int num = (*iter_seq).second.front();
+        std::cout << num << ' ';
+        summ.back() += num;
+        flag = true;
+        (*iter_seq).second.pop_front();
+      }
+    }
+  }
+  auto it = summ.begin();
+  std::cout << *(it++);
+  for (; it != summ.end(); it++)
+  {
+    std::cout << ' ' << *it;
+  }
+  std::cout << '\n';
+  return 0;
 }
