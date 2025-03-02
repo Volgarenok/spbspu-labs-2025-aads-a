f32b57cada04d9f0bc2f6debf6e9036e1b41ff9a
aldyn-d <aldyndirti@gmail.com>
2025-03-02 20:43:24 +0300

    trying input

--- dirti.aldyn/S1/main.cpp
+++ dirti.aldyn/S1/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <list>
+#include <utility>
 
 int main()
 {
+  std::list< std::pair< std::string, std::list< int >>> mainlist;
+  std::string listName;
+  while (std::cin >> listName && !std::cin.eof())
+  {
+    int value = 0;
+    std::list< int > valueList;
+    while (std::cin >> value)
+    {
+      valueList.push_back(value);
+    }
+    mainList.push_back({ listName, valueList });
+  }
+  
 }
