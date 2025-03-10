800398bd04127a27e88fb5ab41f5025e74de6c87
ysffmn <andrewnikonov387@gmail.com>
2025-03-11 01:32:29 +0300

    constructors test done

--- nikonov.andrew/S1/test-List.cpp
+++ nikonov.andrew/S1/test-List.cpp
@@ -17,9 +17,13 @@ BOOST_AUTO_TEST_CASE(ListVariableConstructor_test)
    BOOST_TEST(el == data);
   }
 }
+
+
 BOOST_AUTO_TEST_CASE(ListCopyConstructor_test)
 {
-  nikonov::List< int > list(10, 100);
+  constexpr size_t size = 10;
+  constexpr int data = 100;
+  nikonov::List< int > list(size, data);
   nikonov::List< int > copyList(list);
   BOOST_TEST(list.size() == copyList.size());
   auto it = list.begin();
@@ -27,5 +31,41 @@ BOOST_AUTO_TEST_CASE(ListCopyConstructor_test)
   for (; it != list.end(); ++it, ++copyIt)
   {
     BOOST_TEST(*it == *copyIt);
+  }
+}
+BOOST_AUTO_TEST_CASE(ListMoveConstructor_test)
+{
+  constexpr size_t size = 10;
+  constexpr int data = 100;
+  nikonov::List< int > list(10, 100);
+  nikonov::List< int > copyList(std::move(list));
+  BOOST_TEST(copyList.size() == size);
+  for (auto el : copyList)
+  {
+    BOOST_TEST(el == data);
+  }
+}
+BOOST_AUTO_TEST_CASE(ListInitLConstructor_test)
+{
+  constexpr int data1 = 10;
+  constexpr int data2 = 100;
+  nikonov::List< int > list{ data1, data2 };
+  BOOST_TEST(list.front() == data1);
+  BOOST_TEST(list.back() == data2);
+}
+BOOST_AUTO_TEST_CASE(ListRangeConstructor_test)
+{
+  using cIter = nikonov::ConstListIterator< int >;
+  constexpr size_t size = 10;
+  constexpr int data = 100;
+  nikonov::List< int > list(size, data);
+  cIter begin = list.cbegin();
+  cIter end = list.cend();
+  nikonov::List< int > newList(begin, end);
+  cIter newBegin = newList.cbegin();
+  cIter newEnd = newList.cend();
+  for (; newBegin != newEnd; ++newBegin, ++begin)
+  {
+    BOOST_TEST(*begin == *newBegin);
   }
 }
