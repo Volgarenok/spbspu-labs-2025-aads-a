41e842f042b65171508c4eed65045f00a93faad4
ysffmn <andrewnikonov387@gmail.com>
2025-03-10 17:56:04 +0300

    transfer commit (prev. - technical commit)

--- nikonov.andrew/S1/test-List.cpp
+++ nikonov.andrew/S1/test-List.cpp
@@ -2,8 +2,25 @@
 #include <sstream>
 #include "List.hpp"
 
-BOOST_AUTO_TEST_CASE(List_test)
+BOOST_AUTO_TEST_CASE(ListStandardConstructor_test)
 {
   nikonov::List< int > list;
   BOOST_TEST(list.size() == 0);
+}
+BOOST_AUTO_TEST_CASE(ListVariableConstructor_test)
+{
+  constexpr size_t size = 10;
+  constexpr int data = 100;
+  nikonov::List< int > list(size, data);
+  BOOST_TEST(list.size() == size);
+  for (el : list)
+  {
+    BOOST_TEST(el == data);
+  }
+}
+BOOST_AUTO_TEST_CASE(ListCopyConstructor_test)
+{
+  nikonov::List< int > list(10, 100);
+  nikonov::List< int > copyList(list);
+  BOOST_TEST(list.size() == copyList.size());
 }
