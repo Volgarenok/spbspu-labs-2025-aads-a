ff0a7220b53427e7a198ae02f9bee678737fec52
ysffmn <andrewnikonov387@gmail.com>
2025-03-25 23:28:30 +0300

    raw structures included

--- nikonov.andrew/S2/queue.hpp
+++ nikonov.andrew/S2/queue.hpp
@@ -5,7 +5,8 @@
 #include <cassert>
 namespace nikonov
 {
-  constexpr double reallocScale_ = 1.5; 
+  constexpr size_t defaultCapacity = 10;
+  constexpr double reallocScale = 1.5; 
 
   template< typename T >
   class Queue
@@ -64,17 +65,20 @@ void nikonov::Queue< T >::reallocate(size_t newCap)
 
 template< typename T >
 nikonov::Queue< T >::Queue():
-  arr_(nullptr), size_(0), capacity_(0)
+  arr_(nullptr),
+  size_(0),
+  capacity_(defaultCapacity)
 {}
 template< typename T >
 nikonov::Queue< T >::Queue(const Queue & copy):
-  size_(copy.size_), capacity_(copy.capacity_)
+  arr_(new T * [copy.capacity_]),
+  size_(copy.size_),
+  capacity_(copy.capacity_)
 {
-  reallocate(capacity_);
   size_t cnt = 0;
   try
   {
-    for (; cnt < size_; )
+    for (; cnt < size_; ++cnt)
     {
       arr_[cnt] = new T(copy.arr_[cnt]);
     }
@@ -86,10 +90,12 @@ nikonov::Queue< T >::Queue(const Queue & copy):
 }
 template< typename T >
 nikonov::Queue< T >::Queue(Queue && copy):
-  arr_(copy.arr_), size_(copy.size_), capacity_(copy.capacity_)
+  arr_(copy.arr_),
+  size_(copy.size_),
+  capacity_(copy.capacity_)
 {
-  copy.arr = nullptr;
-  copy.size = 0;
+  copy.arr_ = nullptr;
+  copy.size_ = 0;
 }
 template< typename T >
 nikonov::Queue< T >::~Queue()
@@ -124,13 +130,13 @@ template< typename T >
 T & nikonov::Queue< T >::back() noexcept
 {
   assert(size_ != 0);
-  return arr_[size - 1];
+  return arr_[size_ - 1];
 }
 template< typename T >
 const T & nikonov::Queue< T >::back() const noexcept
 {
   assert(size_ != 0);
-  return arr_[size - 1];
+  return arr_[size_ - 1];
 }
 template< typename T >
 void nikonov::Queue< T >::push(const T & val)
@@ -142,15 +148,22 @@ void nikonov::Queue< T >::push(T && val)
 {
   if (size_ == capacity_)
   {
-    reallocate(capacity_ * reallocScale_);
+    reallocate(capacity_ * reallocScale);
   }
-  arr_[size] = new T(val);
-  ++size;
+  arr_[size_] = new T(val);
+  ++size_;
 }
 template< typename T >
 template< typename... Args >
-void nikonov::Queue< T >::emplace(Args &&...args) //Args && - паттерн раскрытия пакета аргументов
+void nikonov::Queue< T >::emplace(Args &&...args)
 {
+  if (size_ == capacity_)
+  {
+    reallocate(capacity_ * reallocScale);
+  }
+  T * data = new T(std::forward< Args >(args)...);
+  arr_[size_] = data;
+  ++size_;
 }
 template< typename T >
 void nikonov::Queue< T >::pop() noexcept
@@ -158,6 +171,9 @@ void nikonov::Queue< T >::pop() noexcept
   assert(size_ != 0);
   delete *arr_;
   --size_;
+  for (size_t i = 0; i < size_; ++i)
+  {
+    arr_[i] = arr_[i + 1];
+  }
 }
-
 #endif
