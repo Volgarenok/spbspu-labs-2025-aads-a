5878d7232374d2a7d979d0af925490ffc8f714ea
MrMels625 <MrMels625@gmail.com>
2025-04-26 18:05:55 +0300

    Whitespaces fix aaaaaa

--- aleksandrov.igor/S4/node.hpp
+++ aleksandrov.igor/S4/node.hpp
@@ -6,11 +6,11 @@
 namespace aleksandrov
 {
   namespace detail
-  { 
+  {
     template< typename Key, typename Value >
     struct Node
     {
-      std::pair< Key, Value > data[2]; 
+      std::pair< Key, Value > data[2];
       Node* left;
       Node* middle;
       Node* right;
@@ -18,15 +18,15 @@ namespace aleksandrov
 
       Node(const Key& key, const Value& value):
         data{ std::make_pair(key, value), std::pair< Key, Value >() },
-	left(nullptr),
-	middle(nullptr),
-	right(nullptr),
-	parent(nullptr)
+        left(nullptr),
+        middle(nullptr),
+        right(nullptr),
+        parent(nullptr)
       {}
 
       Node(const Key& key1, const Value& value1, const Key& key2, const Value& value2):
         data{ std::make_pair(key1, value1), std::make_pair(key2, value2) },
-	left(nullptr),
+        left(nullptr),
         middle(nullptr),
         right(nullptr),
         parent(nullptr)
