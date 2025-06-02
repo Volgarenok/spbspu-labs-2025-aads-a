206d83298582e9eabc5dfcb35a5acb3dafd11153
Robotophile <ksyunya.iv@gmail.com>
2025-06-02 18:06:44 +0300

    functionsss

--- ivanova.ksenia/S1/list.hpp
+++ ivanova.ksenia/S1/list.hpp
@@ -15,9 +15,15 @@ namespace ivanova
   class List
   {
   public:
+    using reference = T&;
     using const_reference = const T&;
     using size_type = std::size_t;
     using node_type = ListNode<T>;
+    using iterator = ListIterator<List, false>;
+    using const_iterator = ListIterator<List, true>;
+
+    friend iterator;
+    friend const_iterator;
 
     List() noexcept : _head(nullptr), _tail(nullptr), _size(0) {}
 
@@ -55,19 +61,48 @@ namespace ivanova
       moved.clear();
       return *this;
     }
+
     void assign(size_type n, const_reference value);
     template <typename IterType>
     void assign(IterType first, IterType last);
 
     void push_back(const_reference value) { save_push(value, true); }
     void push_front(const_reference value) { save_push(value, false); }
+    void pop_back() { pop(true); }
+    void pop_front() { pop(false); }
 
+    void swap(List& other) noexcept
+    {
+      std::swap(_head, other._head);
+      std::swap(_tail, other._tail);
+      std::swap(_size, other._size);
+    }
     void clear();
+
+    reference front() { return *begin(); }
+    const_reference front() const { return *begin(); }
+
+    reference back() { return *(--end()); }
+    const_reference back() const { return *(--end()); }
+
+    size_type size() const { return _size; }
+    bool empty() const { return size() == 0; }
+
+    iterator begin() { return iterator(this, _head); }
+    const_iterator begin() const { return const_iterator(this, _head); }
+    const_iterator cbegin() const { return const_iterator(this, _head); }
+
+    iterator end() { return iterator(this, nullptr); }
+    const_iterator end() const { return const_iterator(this, nullptr); }
+    const_iterator cend() const { return const_iterator(this, nullptr); }
 
     private:
       node_type* _head;
       node_type* _tail;
       size_type _size;
+
+      void save_push(const_reference value, bool back);
+      void pop(bool back);
   };
   
   template <typename T>
