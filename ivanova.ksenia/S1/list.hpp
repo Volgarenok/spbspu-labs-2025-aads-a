283302f13014c3f6edaadf76d83fef2f35be109c
Robotophile <ksyunya.iv@gmail.com>
2025-06-02 17:54:03 +0300

    some functions for list

--- ivanova.ksenia/S1/list.hpp
+++ ivanova.ksenia/S1/list.hpp
@@ -15,6 +15,7 @@ namespace ivanova
   public:
     using const_reference = const T&;
     using size_type = std::size_t;
+    using node_type = ListNode<T>;
 
     List() noexcept : _head(nullptr), _tail(nullptr), _size(0) {}
 
@@ -45,6 +46,84 @@ namespace ivanova
       }
       return *this;
     }
+
+    List& operator=(List&& moved) noexcept
+    {
+      swap(moved);
+      moved.clear();
+      return *this;
+    }
+    void assign(size_type n, const_reference value);
+    template <typename IterType>
+    void assign(IterType first, IterType last);
+
+    private:
+      node_type* _head;
+      node_type* _tail;
+      size_type _size;
   };
+  
+  template <typename T>
+  void List<T>::assign(size_type n, const_reference value)
+  {
+    clear();
+    try
+    {
+      for (size_type i = 0; i < n; ++i)
+      {
+        push_back(value);
+      }
+    }
+    catch(...)
+    {
+      clear();
+      throw;
+    }
+  }
+
+  template <typename T>
+  template <typename IterType>
+  void List<T>::assign(IterType first, IterType last)
+  {
+    bool same = std::is_same<IterType, iterator>::value
+           || std::is_same<IterType, const_iterator>::value;
+    if (!same || first._list != this)
+    {
+      clear();
+      try
+      {
+        for (IterType x = first; x != last; ++x)
+        {
+          push_back(*x);
+        }
+      }
+      catch(...)
+      {
+        clear();
+        throw;
+      }
+    }
+    else
+    {
+      if (first._node == nullptr)
+      {
+        clear();
+        return;
+      }
+      while (!empty() && _head != first._node)
+      {
+        pop_front();
+      }
+      if (last._node == nullptr)
+      {
+        return;
+      }
+      while (!empty() && _tail != last._node)
+      {
+        pop_back();
+      }
+      pop_back();
+    } 
+  }
 }
 #endif
