a6abc5a5ec7aab33a9a2d92c874d9f75e1f20c5f
Robotophile <ksyunya.iv@gmail.com>
2025-06-02 17:40:03 +0300

    iterator

--- ivanova.ksenia/S1/iterator.hpp
+++ ivanova.ksenia/S1/iterator.hpp
@@ -2,6 +2,7 @@
 #define ITERATOR_HPP
 
 #include <iterator>
+#include <type_traits>
 #include "node.hpp"
 
 namespace ivanova
@@ -11,6 +12,15 @@ namespace ivanova
   {
     friend List;
   public:
+
+    using ptr_list = std::conditional_t<IsConst, const List*, List*>;
+    using ptr_node = std::conditional_t<IsConst, const typename List::node_type*, typename List::node_type*>;
+    using iterator_category = std::bidirectional_iterator_tag;
+    using value_type = typename List::value_type;
+    using difference_type = std::ptrdiff_t;
+    using pointer = std::conditional_t<IsConst, typename List::const_pointer, typename List::pointer>;
+    using reference = std::conditional_t<IsConst, typename List::const_reference, typename List::reference>;
+
     ListIterator() : _list(nullptr), _node(nullptr) {}
     ListIterator(ptr_list list, ptr_node node) : _list(list), _node(node) {}
     ListIterator(const ListIterator& other) : _list(other._list), _node(other._node) {}
@@ -43,10 +53,19 @@ namespace ivanova
 
     ListIterator& operator--()
     {
+      if (_list == nullptr)
+      {
+        return *this;
+      }
+      _node = (_node != nullptr) ? _node->prev : _list->_tail;
+      return *this;
     }
 
     ListIterator operator--(int)
     {
+      ListIterator tmp(*this);
+      --(*this);
+      return tmp;
     }
 
     bool operator==(const ListIterator& rhs) const { return _list == rhs._list && _node == rhs._node; }
