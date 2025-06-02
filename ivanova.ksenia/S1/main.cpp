0403446a4a0ac3794c996965fd6213dcae484b1d
Robotophile <ksyunya.iv@gmail.com>
2025-06-02 18:41:51 +0300

    main

--- ivanova.ksenia/S1/main.cpp
+++ ivanova.ksenia/S1/main.cpp
@@ -1,6 +1,19 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include "list.hpp"
+
+using ivanova::List;
+
+struct NamedList
+{
+  std::string name;
+  List<int> list;
+  List<int>::iterator pos;
+
+  NamedList(const std::string& name) : name(name) {}
+};
 
 int main()
 {
-  
 }
