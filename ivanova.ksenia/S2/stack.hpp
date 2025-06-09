519cb479cdb32d3adaef7a56bbdd43dcdc3892f0
Robotophile <ksyunya.iv@gmail.com>
2025-06-09 21:06:31 +0300

    stack

--- ivanova.ksenia/S2/stack.hpp
+++ ivanova.ksenia/S2/stack.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <algorithm>
 #include <cstddef>
-#include <memory>
+#include "array.hpp"
 
 namespace ivanova
 {
@@ -9,6 +9,13 @@ namespace ivanova
   class Stack
   {
   public:
+    using value_type = T;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using difference_type = std::ptrdiff_t;
+    using size_type = std::size_t;
 
     Stack() = default;
 
@@ -39,6 +46,15 @@ namespace ivanova
     {
       _buffer.swap(other._buffer);
     }
+
+    reference top() { return _buffer.back(); }
+    const_reference top() const { return _buffer.back(); }
+
+    void push(const_reference value) { _buffer.push_back(value); }
+    void pop() { _buffer.pop_back(); }
+
+    size_type size() const noexcept { return _buffer.size(); }
+    bool empty() const noexcept { return _buffer.empty(); }
 
   private:
     Array<T> _buffer;
