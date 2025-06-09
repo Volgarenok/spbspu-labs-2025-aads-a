ddec5ea2f33cd868754e2a30d4462cf666cf465f
Robotophile <ksyunya.iv@gmail.com>
2025-06-09 21:40:56 +0300

    main

--- ivanova.ksenia/S2/main.cpp
+++ ivanova.ksenia/S2/main.cpp
@@ -7,6 +7,57 @@
 #include "expression.hpp"
 #include "stack.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc > 2)
+  {
+    std::cerr << "usage: " << argv[0] << " [filename]\n";
+    return 1;
+  }
+
+  std::ifstream fin;
+  if (argc == 2)
+  {
+    fin.open(argv[1]);
+    if (!fin.is_open())
+    {
+      std::cerr << "can't open file '" << argv[1] << "'\n";
+      return 1;
+    }
+  }
+  std::istream& input = fin.is_open() ? fin : std::cin;
+
+  ivanova::Stack<int64_t> result;
+  try
+  {
+    std::string line;
+    while (std::getline(input, line))
+    {
+      if (line.empty())
+      {
+        continue;
+      }
+      int64_t value = ivanova::countExpression(line);
+      result.push(value);
+    }
+  }
+  catch (std::logic_error& ex)
+  {
+    std::cerr << ex.what() << "\n";
+    return 1;
+  }
+
+  if (!result.empty())
+  {
+    std::cout << result.top();
+    result.pop();
+  }
+  while (!result.empty())
+  {
+    std::cout << " " << result.top();
+    result.pop();
+  }
+  std::cout << "\n";
+
+  return 0;
 }
