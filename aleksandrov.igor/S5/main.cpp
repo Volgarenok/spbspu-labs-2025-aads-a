002641e5c533faf04a60035c2599322239eeb620
MrMels625 <MrMels625@gmail.com>
2025-07-28 13:33:12 +0300

    Command line args parsing and command execution

--- aleksandrov.igor/S5/main.cpp
+++ aleksandrov.igor/S5/main.cpp
@@ -1,3 +1,40 @@
-int main()
-{}
+#include <functional>
+#include <iostream>
+#include <fstream>
+#include <map>
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "ERROR: Incorrect arguments!\n";
+    return 1;
+  }
+
+  std::string command(argv[1]);
+
+  std::ifstream file(argv[2]);
+  if (!file)
+  {
+    std::cerr << "ERROR: Incorrect file!\n";
+    return 1;
+  }
+
+  std::map< std::string, std::function< void() > > commands;
+
+  try
+  {
+    commands.at(command)();
+    std::cout << '\n';
+  }
+  catch (const std::out_of_range&)
+  {
+    std::cout << "<INVALID COMMAND>\n";
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
+}
 
