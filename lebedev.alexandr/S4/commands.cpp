40b547132a5608a33c10fdb6d780161d152f77dd
mokkenz1 <lam1600@mail.ru>
2025-09-20 01:29:00 +0300

    done commands.cpp

--- lebedev.alexandr/S4/commands.cpp
+++ lebedev.alexandr/S4/commands.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 
+
 void lebedev::printCMD(std::ostream& out, dictOfDicts dict)
 {
   std::string name = "";
@@ -13,4 +14,67 @@ void lebedev::printCMD(std::ostream& out, dictOfDicts dict)
     return;
   }
   out << name;
+  for (auto i = current.cbegin(); i != current.cend(); ++i)
+  {
+    out << " " << (*i).first << " " << (*i).second;
+  }
+  out << "\n";
+}
+
+void lebedev::complementCMD(dictOfDicts& dict)
+{
+  std::string name, dictName1, dictName2;
+  std::cin >> name >> dictName1 >> dictName2;
+  auto dict1 = dict.at(dictName1);
+  auto dict2 = dict.at(dictName2);
+  std::map< int, std::string > temp;
+  for (auto i = dict1.begin(); i != dict1.end(); ++i)
+  {
+    int key = (*i).first;
+    if (dict2.find(key) == dict2.end())
+    {
+      temp.insert(*i);
+    }
+  }
+  dict[name] = temp;
+}
+
+void lebedev::intersectCMD(dictOfDicts& dict)
+{
+  std::string name, dictName1, dictName2;
+  std::cin >> name >> dictName1 >> dictName2;
+  auto dict1 = dict.at(dictName1);
+  auto dict2 = dict.at(dictName2);
+  std::map< int, std::string > temp;
+  for (auto i = dict1.begin(); i != dict1.end(); ++i)
+  {
+    int key = (*i).first;
+    if (dict2.find(key) != dict2.end())
+    {
+      temp.insert(*i);
+    }
+  }
+  dict[name] = temp;
+}
+
+void lebedev::unionCMD(dictOfDicts& dict)
+{
+  std::string name, dictName1, dictName2;
+  std::cin >> name >> dictName1 >> dictName2;
+  auto dict1 = dict.at(dictName1);
+  auto dict2 = dict.at(dictName2);
+  std::map< int, std::string > temp;
+  for (auto i = dict1.begin(); i != dict1.end(); ++i)
+  {
+    temp.insert(*i);
+  }
+  for (auto i = dict2.begin(); i != dict2.end(); ++i)
+  {
+    int key = (*i).first;
+    if (dict1.find(key) == dict1.end())
+    {
+      temp.insert(*i);
+    }
+  }
+  dict[name] = temp;
 }
