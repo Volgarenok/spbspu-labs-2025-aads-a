022683896b38eece989ee1f53e8329a424525ce1
mokkenz1 <lam1600@mail.ru>
2025-09-20 05:08:05 +0300

    done AVL tree

---
+++ lebedev.alexandr/common/AVLtree.hpp
@@ -0,0 +1,709 @@
+#ifndef AVLTREE_HPP
+#define AVLTREE_HPP
+#include <utility>
+#include <functional>
+#include "treeNode.hpp"
+#include "iterator.hpp"
+#include "constIterator.hpp"
+
+namespace lebedev
+{
+  template< class Key, class Value, class Cmp = std::less< Key > >
+  class AVLtree
+  {
+  public:
+    using node_t = TreeNode< Key, Value >;
+    using iter = TreeIterator< Key, Value, Cmp >;
+    using cIter = ConstTreeIterator< Key, Value, Cmp >;
+
+    AVLtree();
+    AVLtree(const AVLtree& other);
+    AVLtree(AVLtree&& other);
+    AVLtree< Key, Value, Cmp >& operator=(const AVLtree& other);
+    AVLtree< Key, Value, Cmp>& operator=(AVLtree&& other);
+
+    iter begin() noexcept;
+    iter end() noexcept;
+    cIter cBegin() const noexcept;
+    cIter cEnd() const noexcept;
+
+    iter find(const Key&);
+    cIter find(const Key&) const;
+    void swap(AVLtree< Key, Value, Cmp >&) noexcept;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    iter erase(iter) noexcept;
+    size_t erase(const Key&) noexcept;
+    size_t count(const Key&) const noexcept;
+
+    Value& at(const Key &);
+    const Value& at(const Key &) const;
+    std::pair< iter, bool > insert(const std::pair< Key, Value >&);
+    Value& operator[](const Key&);
+
+    ~AVLtree();
+  private:
+    node_t* fakeroot_;
+    node_t* nil_
+    node_t* root_;
+    Cmp cmp_;
+    size_t size_;
+
+    node_t* copyTree(node_t* root);
+    void clear(node_t* root) noexcept;
+    void updateHeight(node_t* root) noexcept;
+    node_t* rotateRight(node_t* root) noexcept;
+    node_t* rotateLeft(node_t* root) noexcept;
+    node_t* rotateRightLeft(node_t* root) noexcept;
+    node_t* rotateLeftRight(node_t* root) noexcept;
+    int getBalance(node_t* root) noexcept;
+    node_t* balance(node_t* root) noexcept;
+    std::pair< iter, bool > treeInsert(const Key&, const Value&);
+    node_t* findMin(node_t* node) noexcept;
+    node_t* findMax(node_t* node) noexcept;
+    node_t* findSuccessor(node_t* node) noexcept;
+    node_t* deleteNode(node_t* node, const Key&) noexcept;
+  };
+
+  template< class Key, class Value, class Cmp >
+  AVLtree< Key, Value, Cmp >::AVLtree():
+    nil_(new node_t(Key(), Value(), nullptr)),
+    fakeroot_(new node_t(Key(), Value(), nullptr)),
+    root_(nullptr),
+    cmp_(),
+    size_(0)
+  {
+    nil_->height = 0;
+    nil_->left = nullptr;
+    nil_->right = nullptr;
+    fakeroot_->left = nil_;
+    fakeroot_->right = nil_;
+    fakeroot_->parent = nullptr;
+  }
+
+  template< class Key, class Value, class Cmp >
+  AVLtree< Key, Value, Cmp >::AVLtree(const AVLtree& other):
+    nil_(new node_t(Key(), Value(), nullptr)),
+    fakeroot_(new node_t(Key(), Value(), nullptr)),
+    cmp_(other.cmp_),
+    size_(other.size_)
+  {
+    try
+    {
+      fakeroot_->left = copyTree(other.fakeroot_->left);
+      root_ = fakeroot_->left;
+      if (root_)
+      {
+        root_->parent = fakeroot_;
+      }
+    }
+    catch(...)
+    {
+      delete fakeroot_;
+      throw;
+    }
+  }
+
+  template< class Key, class Value, class Cmp >
+  AVLtree< Key, Value, Cmp >::AVLtree(AVLtree&& other):
+    nil_(other.nil_),
+    fakeroot_(other.fakeroot_),
+    root_(other.root_),
+    cmp_(other.cmp_),
+    size_(other.size_)
+  {
+    other.nil_ = new node_t(Key(), Value(), nullptr);
+    other.nil_->height = 0;
+    other.nil_->left = nullptr;
+    other.nil_->right = nullptr;
+    other.fakeroot_ = new node_t(Key(), Value(), nullptr);
+    other.fakeroot_->left = other.nil_;
+    other.fakeroot_->right = other.nil_;
+    other.root_ = other.nil_;
+    other.size_ = 0;
+  }
+
+  template< class Key, class Value, class Cmp >
+  AVLtree< Key, Value, Cmp >& AVLtree< Key, Value, Cmp >::operator=(AVLtree&& other)
+  {
+    if (this == std::addressof(other))
+    {
+      return *this;
+    }
+    clear(root_);
+    delete fakeroot_;
+    delete nil_;
+    nil_ = other.nil_;
+    fakeroot_ = other.fakeroot_;
+    root_ = other.root_;
+    cmp_ = std::move(other.cmp_);
+    size_ = other.size_;
+    other.nil_ = new node_t(Key(), Value(), nullptr);
+    other.nil_->height = 0;
+    other.nil_->left = nullptr;
+    other.nil_->right = nullptr;
+    other.fakeroot_ = new node_t(Key(), Value(), nullptr);
+    other.fakeroot_->left = other.nil_;
+    other.fakeroot_->right = other.nil_;
+    other.root_ = other.nil_;
+    other.size_ = 0;
+    return *this;
+  }
+
+  template< class Key, class Value, class Cmp >
+  AVLtree< Key, Value, Cmp >& AVLtree< Key, Value, Cmp>::operator=(const AVLtree& other)
+  {
+    if (this == std::addressof(other))
+    {
+      return *this;
+    }
+    AVLtree temp = other;
+    swap(temp);
+    return *this;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::iter AVLtree< Key, Value, Cmp >::begin() noexcept
+  {
+    node_t* temp = root_;
+    while (temp->left != nil_)
+    {
+      temp = temp->left;
+    }
+    return iter(temp);
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::iter AVLtree< Key, Value, Cmp >::end() noexcept
+  {
+    return iter(fakeroot_);
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::cIter AVLtree< Key, Value, Cmp >::cBegin() const noexcept
+  {
+    const node_t* temp = root_;
+    while (temp->left != nil_)
+    {
+      temp = temp->left;
+    }
+    return cIter(temp);
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::cIter AVLtree< Key, Value, Cmp >::cEnd() const noexcept
+  {
+    return cIter(fakeroot_);
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::iter AVLtree< Key, Value, Cmp >::find(const Key& k)
+  {
+    node_t* current = fakeroot_->left;
+    while (current != nil_)
+    {
+      if (cmp_(k, current->data.first))
+      {
+        current = current->left;
+      }
+      else if (cmp_(current->data.first, k))
+      {
+        current = current->right;
+      }
+      else
+      {
+        return iter(current);
+      }
+    }
+    return end();
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::cIter AVLtree< Key, Value, Cmp >::find(const Key& k) const
+  {
+    const node_t* current = fakeroot_->left;
+    while (current != nil_)
+    {
+      if (cmp_(k, current->data.first))
+      {
+        current = current->left;
+      }
+      else if (cmp_(current->data.first, k))
+      {
+        current = current->right;
+      }
+      else
+      {
+        return cIter(current);
+      }
+    }
+    return cEnd();
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::copyTree(node_t* root)
+  {
+    if (!root)
+    {
+      return nullptr;
+    }
+    node_t* leftPart = nullptr;
+    node_t* rightPart = nullptr;
+    try
+    {
+      leftPart = copyTree(root->left);
+      rightPart = copyTree(root->right);
+      node_t* newNode = new node_t(root->data.first, root->data.second, nullptr);
+      newNode->left = leftPart;
+      newNode->right = rightPart;
+      newNode->height = root->height;
+      if (leftPart)
+      {
+        leftPart->parent = newNode;
+      }
+      if (rightPart)
+      {
+        rightPart->parent = newNode;
+      }
+      return newNode;
+    }
+    catch (...)
+    {
+      clear(leftPart);
+      clear(rightPart);
+      throw;
+    }
+  }
+
+  template< class Key, class Value, class Cmp >
+  void AVLtree< Key, Value, Cmp >::updateHeight(node_t* root) noexcept
+  {
+    if (root == nil_)
+    {
+      return;
+    }
+    int leftHeight = (root->left != nil_) ? root->left->height : 0;
+    int rightHeight = (root->right != nil_) ? root->right->height : 0;
+    root->height = 1 + std::max(leftHeight, rightHeight);
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::rotateRight(node_t* root) noexcept
+  {
+    if (root == nil_ || root->left == nil_)
+    {
+      return root;
+    }
+    node_t* newRoot = root->left;
+    root->left = newRoot->right;
+    if (newRoot->right)
+    {
+      newRoot->right->parent = root;
+    }
+    newRoot->parent = root->parent;
+    if (root == fakeroot_->left)
+    {
+      fakeroot_->left = newRoot;
+      root_ = newRoot;
+    }
+    else if (root->parent)
+    {
+      if (root == root->parent->left)
+      {
+        root->parent->left = newRoot;
+      }
+      else
+      {
+        root->parent->right = newRoot;
+      }
+    }
+    newRoot->right = root;
+    root->parent = newRoot;
+    updateHeight(root);
+    updateHeight(newRoot);
+    return newRoot;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::rotateLeft(node_t* root) noexcept
+  {
+    if (root == nil_ || root->right == nil_)
+    {
+      return root;
+    }
+    node_t* newRoot = root->right;
+    root->right = newRoot->left;
+    if (newRoot->left)
+    {
+      newRoot->left->parent = root;
+    }
+    newRoot->parent = root->parent;
+    if (root == fakeroot_->left)
+    {
+      fakeroot_->left = newRoot;
+      root_ = newRoot;
+    }
+    else if (root->parent)
+    {
+      if (root == root->parent->left)
+      {
+        root->parent->left = newRoot;
+      }
+      else
+      {
+        root->parent->right = newRoot;
+      }
+    }
+    newRoot->left = root;
+    root->parent = newRoot;
+    updateHeight(root);
+    updateHeight(newRoot);
+    return newRoot;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::rotateRightLeft(node_t* root) noexcept
+  {
+    if (root == nil_ || root->right == nil_)
+    {
+      return root;
+    }
+    root->right = rotateRight(root->right);
+    if (root->right)
+    {
+      root->right->parent = root;
+    }
+    return rotateLeft(root);
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::rotateLeftRight(node_t* root) noexcept
+  {
+    if (root == nil_ || root->left == nil_)
+    {
+      return root;
+    }
+    root->left = rotateLeft(root->left);
+    if (root->left)
+    {
+      root->left->parent = root;
+    }
+    return rotateRight(root);
+  }
+
+  template< class Key, class Value, class Cmp >
+  int AVLtree< Key, Value, Cmp >::getBalance(node_t* root) noexcept
+  {
+    if (root == nil_)
+    {
+      return 0;
+    }
+    int leftH = (root->left != nil_) ? root->left->height : 0;
+    int rightH = (root->right != nil_) ? root->right->height : 0;
+    return leftH - rightH;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp>::node_t* AVLtree< Key, Value, Cmp >::balance(node_t* root) noexcept
+  {
+    if (!root || root == fakeroot_)
+    {
+      return root;
+    }
+    root->left = balance(root->left);
+    root->right = balance(root->right);
+    updateHeight(root);
+    int balance = getBalance(root);
+
+    if (balance > 1)
+    {
+      if (root->left && getBalance(root->left) >= 0)
+      {
+        return rotateRight(root);
+      }
+      else if (root->left)
+      {
+        return rotateLeftRight(root);
+      }
+    }
+    else if (balance < -1)
+    {
+      if (root->right && getBalance(root->right) <= 0)
+      {
+        return rotateLeft(root);
+      }
+      else if (root->right)
+      {
+        return rotateRightLeft(root);
+      }
+    }
+    return root;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::findMin(node_t* node) noexcept
+  {
+    if (node == nil_)
+    {
+      return nil_;
+    }
+    while(node->left != nil_)
+    {
+      node = node->left;
+    }
+    return node;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::findMax(node_t* node) noexcept
+  {
+    if (node == nil_)
+    {
+      return nil_;
+    }
+    while(node->right != nil_)
+    {
+      node = node->right;
+    }
+    return node;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::findSuccessor(node_t* node) noexcept
+  {
+    if (node == nil_)
+    {
+      return nil_;
+    }
+    if (node->right)
+    {
+      return findMin(node->right);
+    }
+    while (node->parent)
+    {
+      if (node == node->parent->left)
+      {
+        return node->parent;
+      }
+      node = node->parent;
+    }
+    return nullptr;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::node_t* AVLtree< Key, Value, Cmp >::deleteNode(node_t* node, const Key& key) noexcept
+  {
+    if (node == nil_)
+    {
+      return nil_;
+    }
+    if (cmp_(key, node->data.first))
+    {
+      node->left = deleteNode(node->left, key);
+    }
+    else if (cmp_(node->data.first, key))
+    {
+      node->right = deleteNode(node->right, key);
+    }
+    else
+    {
+      if (!node->left || !node->right)
+      {
+        node_t* temp = node->left ? node->left : node->right;
+        if (!temp)
+        {
+          temp = node;
+          node = nullptr;
+        }
+        else
+        {
+          *node = *temp;
+        }
+        delete temp;
+      }
+      else
+      {
+        node_t* temp = findMin(node->right);
+        node->data = temp->data;
+        node->right = deleteNode(node->right, temp->data.first);
+      }
+    }
+    if (!node)
+    {
+      return nullptr;
+    }
+    return balance(node);
+  }
+
+  template< class Key, class Value, class Cmp >
+  std::pair< typename AVLtree< Key, Value, Cmp >::iter, bool > AVLtree< Key, Value, Cmp >::treeInsert(const Key& k, const Value& v)
+  {
+    if (empty())
+    {
+      fakeroot_->left = new node_t{ k, v, fakeroot_ };
+      root_ = fakeroot_->left;
+      root_->left = nil_;
+      root_->right = nil_;
+      ++size_;
+      return { iter(root_), true };
+    }
+    node_t* current = fakeroot_->left;
+    node_t* parent = fakeroot_;
+    while (current != nil_)
+    {
+      if (cmp_(k, current->data.first))
+      {
+        parent = current;
+        current = current->left;
+      }
+      else if (cmp_(current->data.first, k))
+      {
+        parent = current;
+        current = current->right;
+      }
+      else
+      {
+        return { iter(current), false };
+      }
+    }
+    node_t* newNode = new node_t(k, v, parent);
+    newNode->left = nil_;
+    newNode->right = nil_;
+    if (cmp_(k, parent->data.first))
+    {
+      parent->left = newNode;
+    }
+    else
+    {
+      parent->right = newNode;
+    }
+    ++size_;
+    balance(fakeroot_->left);
+    return { iter(newNode), true };
+  }
+
+  template< class Key, class Value, class Cmp >
+  std::pair< typename AVLtree< Key, Value, Cmp >::iter, bool > AVLtree< Key, Value, Cmp >::insert(const std::pair< Key, Value >& elem)
+  {
+    return treeInsert(elem.first, elem.second);
+  }
+
+  template< class Key, class Value, class Cmp >
+  Value& AVLtree< Key, Value, Cmp >::at(const Key& k)
+  {
+    iter i = find(k);
+    if (i == end())
+    {
+      throw std::out_of_range("KEY NOT FOUND");
+    }
+    return (*i).second;
+  }
+
+  template< class Key, class Value, class Cmp >
+  const Value& AVLtree< Key, Value, Cmp >::at(const Key& k) const
+  {
+    cIter i = find(k);
+    if (i == cEnd())
+    {
+      throw std::out_of_range("KEY NOT FOUND");
+    }
+    return (*i).second;
+  }
+
+  template< class Key, class Value, class Cmp >
+  Value& AVLtree< Key, Value, Cmp >::operator[](const Key& k)
+  {
+    iter i = find(k);
+    if (i == end())
+    {
+      insert(std::make_pair(k, Value()));
+      i = find(k);
+    }
+    return (*i).second;
+  }
+
+  template< class Key, class Value, class Cmp >
+  void AVLtree< Key, Value, Cmp >::swap(AVLtree< Key, Value, Cmp >& other) noexcept
+  {
+    std::swap(fakeroot_, other.fakeroot_);
+    std::swap(root_, other.root_);
+    std::swap(cmp_, other.cmp_);
+    std::swap(size_, other.size_);
+  }
+
+  template< class Key, class Value, class Cmp >
+  bool AVLtree< Key, Value, Cmp >::empty() const noexcept
+  {
+    return !fakeroot_->left;
+  }
+
+  template< class Key, class Value, class Cmp >
+  size_t AVLtree< Key, Value, Cmp >::size() const noexcept
+  {
+    return size_;
+  }
+
+  template< class Key, class Value, class Cmp >
+  typename AVLtree< Key, Value, Cmp >::iter AVLtree< Key, Value, Cmp >::erase(iter it) noexcept
+  {
+    if (it == end())
+    {
+      return end();
+    }
+    node_t* node = it.node_;
+    node_t* next = findSuccessor(node);
+    fakeroot_->left = deleteNode(root_, node->data.first);
+    if (fakeroot_->left)
+    {
+      fakeroot_->left->parent = fakeroot_;
+    }
+    --size_;
+    if (next != nullptr)
+    {
+      return iter(next);
+    }
+    else
+    {
+      return end();
+    }
+  }
+
+  template< class Key, class Value, class Cmp >
+  size_t AVLtree< Key, Value, Cmp >::erase(const Key& key) noexcept
+  {
+    iter it = find(key);
+    if (it != end())
+    {
+      erase(it);
+      return 1;
+    }
+    return 0;
+  }
+
+  template<class Key, class Value, class Cmp>
+  size_t AVLtree<Key, Value, Cmp>::count(const Key& key) const noexcept
+  {
+    return find(key) != cEnd() ? 1 : 0;
+  }
+
+  template< class Key, class Value, class Cmp >
+  void AVLtree< Key, Value, Cmp >::clear(node_t* root) noexcept
+  {
+    if (root && root != nil_)
+    {
+      clear(root->left);
+      clear(root->right);
+      delete root;
+    }
+  }
+
+  template< class Key, class Value, class Cmp >
+  AVLtree< Key, Value, Cmp >::~AVLtree()
+  {
+    clear(fakeroot_->left);
+    delete fakeroot_;
+    delete nil_;
+  }
+}
+
+#endif
