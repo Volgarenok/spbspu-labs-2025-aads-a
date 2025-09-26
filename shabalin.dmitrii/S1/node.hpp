a1388a4f7503c17ad6abef4eeac215e9141c19a8
dmitei <dima9990860349@icloud.com>
2025-09-26 17:20:01 +0300

    hpp prop

--- shabalin.dmitrii/S1/node.hpp
+++ shabalin.dmitrii/S1/node.hpp
@@ -3,7 +3,18 @@
 
 namespace shabalin
 {
+  template < typename T >
+  struct Node
+  {
+    T data_;
+    Node< T > * next_;
 
+    explicit Node(const T & data):
+      data_(data),
+      next_(nullptr)
+    {}
+  };
 }
 
 #endif
+
