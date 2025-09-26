31c77f4950929978a53cb38751cc8f407d0b7c0e
“Karnaukhova <masj280906@gmail.com>
2025-09-27 02:46:02 +0300

    a little iterator

--- karnauhova.alexandra/S7/hashCIterator.hpp
+++ karnauhova.alexandra/S7/hashCIterator.hpp
@@ -13,8 +13,14 @@ namespace karnauhova
     using this_t = HashCIterator< Key, Value, Hash1, Equal >;
   public:
     HashCIterator();
+    HashCIterator(const this_t&);
     this_t& operator++() noexcept;
     this_t operator++(int) noexcept;
+    this_t& operator--() noexcept;
+    this_t operator--(int) noexcept;
+
+    bool operator==(const this_t&) const noexcept;
+    bool operator!=(const this_t&) const noexcept;
 
   private:
     const Table* table_;
@@ -29,15 +35,59 @@ namespace karnauhova
   {}
 
   template< typename Key, typename Value, typename Hash1, typename Equal >
+  HashCIterator< Key, Value, Hash1, Equal >::HashCIterator(const HashCIterator& oth):
+    table_(oth.table_),
+    index_(oth.index_)
+  {}
+
+  template< typename Key, typename Value, typename Hash1, typename Equal >
   HashCIterator< Key, Value, Hash1, Equal >& HashCIterator< Key, Value, Hash1, Equal >::operator++() noexcept
   {
-
+    ++index_;
+    while (index_ < table_->slots_.size() && table_->slots_[index_].status != Table::Status::OCCUPIED)
+    {
+      ++index_;
+    }
+    return *this;
   }
   
   template< typename Key, typename Value, typename Hash1, typename Equal >
   HashCIterator< Key, Value, Hash1, Equal > HashCIterator< Key, Value, Hash1, Equal >::operator++(int) noexcept
   {
-    
+    this_t result(*this);
+    ++(*this);
+    return result;
+  }
+
+  template< typename Key, typename Value, typename Hash1, typename Equal >
+  HashCIterator< Key, Value, Hash1, Equal >& HashCIterator< Key, Value, Hash1, Equal >::operator--() noexcept
+  {
+    --index_;
+    while (index_ > 0 && table_->slots_[index_].status != Table::Status::OCCUPIED)
+    {
+      --index_;
+    }
+    return *this;
+  }
+  
+  template< typename Key, typename Value, typename Hash1, typename Equal >
+  HashCIterator< Key, Value, Hash1, Equal > HashCIterator< Key, Value, Hash1, Equal >::operator--(int) noexcept
+  {
+    this_t result(*this);
+    --(*this);
+    return result;
+  }
+
+  template< typename Key, typename Value, typename Hash1, typename Equal >
+  bool HashCIterator< Key, Value, Hash1, Equal >::operator==(const this_t& oth) const noexcept
+  {
+    return table_ == oth.table_ && index_ == oth.index_;
+  }
+
+  template< typename Key, typename Value, typename Hash1, typename Equal >
+  bool HashCIterator< Key, Value, Hash1, Equal >::operator!=(const this_t& oth) const noexcept
+  {
+    return !(*this == oth);
   }
 }
 
