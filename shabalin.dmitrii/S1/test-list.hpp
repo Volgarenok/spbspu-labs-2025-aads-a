b53a345ccb22aa0cf21dd3e1895f59461b7cca1f
dmitei <dima9990860349@icloud.com>
2025-10-18 15:25:19 +0300

    test2

--- shabalin.dmitrii/S1/test-list.hpp
+++ shabalin.dmitrii/S1/test-list.hpp
@@ -13,7 +13,6 @@ BOOST_AUTO_TEST_CASE(push_back_test)
   BOOST_TEST(list.front() == 10);
   list.push_back(100);
   BOOST_TEST(list.front() == 10);
-  
   auto it = list.begin();
   ++it;
   BOOST_TEST(*it == 100);
@@ -56,7 +55,6 @@ BOOST_AUTO_TEST_CASE(pushFront_test)
   BOOST_TEST(list.front() == 10);
   list.push_front(100);
   BOOST_TEST(list.front() == 100);
-  
   auto it = list.begin();
   ++it;
   BOOST_TEST(*it == 10);
@@ -110,9 +108,7 @@ BOOST_AUTO_TEST_CASE(reverse_test)
   list.push_back(10);
   list.push_back(20);
   list.push_back(30);
-  
   list.reverse();
-  
   auto it = list.begin();
   BOOST_TEST(*it == 30);
   ++it;
@@ -128,9 +124,7 @@ BOOST_AUTO_TEST_CASE(remove_test)
   list.push_back(20);
   list.push_back(10);
   list.push_back(30);
-  
   list.remove(10);
-  
   auto it = list.begin();
   BOOST_TEST(*it == 20);
   ++it;
@@ -145,9 +139,7 @@ BOOST_AUTO_TEST_CASE(remove_if_test)
   list.push_back(25);
   list.push_back(5);
   list.push_back(30);
-  
   list.remove_if([](size_t val) { return val < 20; });
-  
   auto it = list.begin();
   BOOST_TEST(*it == 25);
   ++it;
@@ -160,9 +152,7 @@ BOOST_AUTO_TEST_CASE(assign_test)
   List< size_t > list;
   list.push_back(1);
   list.push_back(2);
-  
   list.assign(3, 100);
-  
   BOOST_TEST(list.front() == 100);
   auto it = list.begin();
   BOOST_TEST(*it == 100);
@@ -176,7 +166,6 @@ BOOST_AUTO_TEST_CASE(assign_test)
 BOOST_AUTO_TEST_CASE(initializer_list_test)
 {
   List< size_t > list = {10, 20, 30};
-  
   auto it = list.begin();
   BOOST_TEST(*it == 10);
   ++it;
@@ -190,21 +179,16 @@ BOOST_AUTO_TEST_CASE(swap_test)
   List< size_t > list1;
   list1.push_back(10);
   list1.push_back(20);
-  
   List< size_t > list2;
   list2.push_back(30);
   list2.push_back(40);
   list2.push_back(50);
-  
   list1.swap(list2);
-  
   BOOST_TEST(list1.front() == 30);
   BOOST_TEST(list2.front() == 10);
-  
   size_t count1 = 0;
   for (auto it = list1.begin(); it != list1.end(); ++it) count1++;
   BOOST_TEST(count1 == 3);
-  
   size_t count2 = 0;
   for (auto it = list2.begin(); it != list2.end(); ++it) count2++;
   BOOST_TEST(count2 == 2);
